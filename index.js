8e8b90953e1522ec718559fc23550527d18d0711
Your Name <minajmiha02@gmail.com>
2024-11-22 11:03:18 +0600

    jwt token

--- index.js
+++ index.js
@@ -6,8 +6,35 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.port || 4000;
 // middleware
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", optionSuccessStatus: 200 }));
 app.use(express.json());
+
+// token verification
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.send({ message: "No Token" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_KEY_TOKEN, (err, decode) => {
+    if (err) {
+      return res.send({ message: "Invalid Token" });
+    }
+    req.decode = decode;
+    next();
+  });
+};
+
+// seller varification
+const verifySellarToken = async (req, res, next) => {
+  const email = req.decode.email;
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  if (user?.role !== "seller") {
+    return res.send({ message: "Forbiden Access" });
+  }
+  next();
+};
 
 // mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.63qrdth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -21,24 +48,46 @@ const client = new MongoClient(uri, {
   },
 });
 // collections
-const productCollection=client.db("WAVE3").collection("products");
-const userCollection=client.db("WAVE3").collection("users");
+const productCollection = client.db("WAVE3").collection("products");
+const userCollection = client.db("WAVE3").collection("users");
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-        //post users
-        app.post("/users", async (req, res) => {
-          const user = req.body;
-          const query = { email: user.email };
-          const existEmail = await userCollection.findOne(query);
-          if (existEmail) {
-            return res.send({ message: "user already exist" });
-          }
-          const result = await userCollection.insertOne(user);
-          res.send(result);
-        });
+    //post users in register
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existEmail = await userCollection.findOne(query);
+      if (existEmail) {
+        return res.send({ message: "user already exist" });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // get useUserData
+    app.get("/user/:email", async (req, res) => {
+      const query = { email: req.params.email };
+      const user = await userCollection.findOne(query);
+      if (!user) {
+        return res.send({ message: "No user found" });
+      }
+      res.send(user);
+    });
+    // add products by seller
+    app.post(
+      "/add-products",
+      verifyJWT,
+      verifySellarToken,
+      async (req, res) => {
+        const product = req.body;
+        const result = await productsCollection.insertOne(product);
+        res.send(result);
+      }
+    );
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -56,7 +105,7 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("server is running by gadget shop");
 });
-// jwt
+// jwt in onAuthStateChange
 app.post("/authentication", (req, res) => {
   const useEmail = req.body;
   const token = jwt.sign(useEmail, process.env.ACCESS_KEY_TOKEN, {
